46d1a661de6d84e8142014bc0e13eb4bda72ee68
ShoheiyoShida <129791157+49822@users.noreply.github.com>
2023-05-18 03:02:19 +0000

    最初の一歩

---
+++ config/routes.rb
@@ -0,0 +1,69 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'products/index'
+    get 'products/new'
+    get 'products/create'
+    get 'products/show'
+    get 'products/edit'
+    get 'products/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'shipping_addresses/index'
+    get 'shipping_addresses/edit'
+    get 'shipping_addresses/create'
+    get 'shipping_addresses/update'
+    get 'shipping_addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/thanks'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroyde'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdrawal'
+  end
+  namespace :public do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+  end
+  namespace :public do
+    get 'products/index'
+    get 'products/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
