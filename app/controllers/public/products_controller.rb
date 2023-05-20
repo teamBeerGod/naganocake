0a7995a2341f85ecb5948979d90560818fc02cb2
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-05-20 08:57:00 +0000

    商品一覧と詳細

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,17 @@
 class Public::ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def show
+    @products = Product.find(params[:id])
   end
+  
+    # 商品データのストロングパラメータ
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price_without_tax)
+  end
+  
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,12 @@ class Product < ApplicationRecord
    belongs_to :order_detail
    has_many :cart_items, dependent: :destroy
 
-
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
   
 end
