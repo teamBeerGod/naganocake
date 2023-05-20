733e96aa9e2f73db4caeadf94c8585c4b0f301cb
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-05-20 08:06:52 +0000

    seedファイル追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,11 @@
 class Product < ApplicationRecord
-  
-  has_one_attached :image 
-  
-   belongs_to :genre
-   belongs_to :order_detail
-   has_many :cart_items, dependent: :destroy
+
+  has_one_attached :image
+
+  belongs_to :genre
+  has_many :order_details, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
 
 
-  
+
 end
