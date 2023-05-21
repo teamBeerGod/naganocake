e1a30c7e1823b045ef42877c586ae8d8237b805d
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-05-21 08:04:40 +0000

    顧客の商品一覧と詳細

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,20 +1,18 @@
 class Public::ProductsController < ApplicationController
   def index
     @products = Product.all
-    #商品が登録できるようになったら下記を有効にする?
-    #@product = Product.find(params[:id])
   end
 
   def show
     @product = Product.find(params[:id])
-    @cart_item = Cart_item.new
+    @cart_item = CartItem.new
   end
   
     # 商品データのストロングパラメータ
   private
 
   def product_params
-    params.require(:product).permit(:name, :price_without_tax)
+    params.require(:product).permit(:name, :price_without_tax, :image)
   end
   
 end
