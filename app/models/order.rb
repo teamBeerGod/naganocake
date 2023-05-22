6ed9f8d3d96ab6afc8b0bbd435fe15013606f322
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-05-22 06:47:55 +0000

    orderコントローラー編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
 
+  enum payment_method: { credit_card: 0, transfer: 1}
+  enum status: {
+    awaiting_payment: 0,
+    confirmed_payment: 1,
+    in_production: 2,
+    preparing_to_ship: 3,
+    done: 4
+  }
+
+
 end
