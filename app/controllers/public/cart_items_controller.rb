c8d359004401df05ce8fddae1bc9adc761350bed
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-05-23 06:03:34 +0000

    カート機能実装中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,37 @@
 class Public::CartItemsController < ApplicationController
   def index
-  end
+#    @cart_items = curremt_customer.cart_items
+    @cart_items = CartItem.all
 
-  def update
-  end
-
-  def destroyd
-  end
-
-  def destroy_all
   end
 
   def create
+    if true
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = 1 #current_customer.id
+      @cart_item.save
+    else
+
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to :index
+  end
+
+  def destroy_all
+    @cart_items = current_customer.cart_item
+    @cart_items.destroy_all
+    redirect_to :index
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:product_id, :quantity)
   end
 end
